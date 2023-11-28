ad16b839ea07a5a610d8d329d790bd24a30e2aa0
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-11-28 16:15:40 +0100

    Game Layout

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -10,8 +10,23 @@ const TicTacToe = () => {
             Tic Tac Toe Game in  <span>React</span>
         </h1>
         <div className='board'>
-            <button className='reset'>Reset</button>
+          <div className='row1'>
+            <div className='boxes'></div>
+            <div className='boxes'></div>
+            <div className='boxes'></div>
+          </div>
+          <div className='row2'>
+            <div className='boxes'></div>
+            <div className='boxes'></div>
+            <div className='boxes'></div>
+          </div>
+          <div className='row3'>
+            <div className='boxes'></div>
+            <div className='boxes'></div>
+            <div className='boxes'></div>
+          </div>
         </div>
+        <button className='reset'>Reset</button>
     </div>
   )
 }
